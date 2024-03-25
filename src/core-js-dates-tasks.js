9906f162a48e005164020c901232d45051d4111f
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-03-25 13:00:04 +0400

    feat: solve some new task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -52,7 +52,9 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(date) {}
+function getDayName(/* date */) {
+  throw new Error('Not implemented');
+}
 
 /**
  * Returns the date of the next Friday from a given date.
@@ -65,7 +67,9 @@ function getDayName(date) {}
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(date) {}
+function getNextFriday(/* date */) {
+  throw new Error('Not implemented');
+}
 
 /**
  * Returns the number of days in a specified month and year.
@@ -230,8 +234,13 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = {
